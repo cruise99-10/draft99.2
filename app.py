fb4758eacc177b34e61a6a826afc89e415eaf54f
Jungki <andy960609@gmail.com>
2022-12-11 11:49:25 +0900

    진짜

--- app.py
+++ app.py
@@ -28,7 +28,35 @@ SECRET_KEY = 'SPARTA'
 # login (상휘)
 
 # review (정기)
+@app.route('/')
+def home():
+    return render_template('review.html')
 
+
+@app.route("/library/write", methods=["POST"])
+def posting():
+    url_receive = request.form['url_give']
+    reviewcomment_receive = request.form['reviewcomment_give']
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    title = soup.select_one('meta[property="og:title"]')['content']
+    image = soup.select_one('meta[property="og:image"]')['content']
+    desc = soup.select_one('meta[property="og:description"]')['content']
+
+    doc ={
+        'title':title,
+        'image':image,
+        'desc':desc,
+        'reviewcomment':reviewcomment_receive
+    }
+    db.reviews.insert_one(doc)
+
+    return jsonify({'msg':'저장 완료!'})
 # commentList (지영)
 
 # myPage (경은)
